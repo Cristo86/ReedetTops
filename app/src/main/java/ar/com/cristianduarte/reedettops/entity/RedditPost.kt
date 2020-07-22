2c525138255d8469e525da6f1b5cc17774e45656
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2020-07-21 21:23:48 -0300

    Initial commit. New Project. Empty item layout.

---
+++ app/src/main/java/ar/com/cristianduarte/reedettops/entity/RedditPost.kt
@@ -0,0 +1,11 @@
+package ar.com.cristianduarte.reedettops.entity
+
+class RedditPost (
+    val id: String,
+    val author: String,
+    val title: String,
+    val numComments: Int,
+    val createdUtc: Long,
+    val thumbnail: String
+
+)
---
+++ app/src/main/java/ar/com/cristianduarte/reedettops/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ar.com.cristianduarte.reedettops.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
