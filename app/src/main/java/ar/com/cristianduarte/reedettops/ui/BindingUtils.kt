0596449384cfdf3331e2330b195ea904d703b6eb
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2020-07-25 15:31:26 -0300

    Show images (Glide library)

--- app/src/main/java/ar/com/cristianduarte/reedettops/ui/BindingUtils.kt
+++ app/src/main/java/ar/com/cristianduarte/reedettops/ui/BindingUtils.kt
@@ -1,9 +1,22 @@
 package ar.com.cristianduarte.reedettops.ui
 
 import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("makeInvisibie")
 fun makeInvisible(view: View, makeInvisibie: Boolean) {
     view.visibility = if (makeInvisibie) View.INVISIBLE else View.VISIBLE
+}
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
 }
