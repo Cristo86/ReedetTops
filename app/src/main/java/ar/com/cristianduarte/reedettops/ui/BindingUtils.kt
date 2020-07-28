5e47544de41e0d44f4715de63b0b8d88da15af0f
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2020-07-28 01:23:51 -0300

    Make image buttons hidden when no image

--- app/src/main/java/ar/com/cristianduarte/reedettops/ui/BindingUtils.kt
+++ app/src/main/java/ar/com/cristianduarte/reedettops/ui/BindingUtils.kt
@@ -1,6 +1,5 @@
 package ar.com.cristianduarte.reedettops.ui
 
-import android.app.Application
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,8 +13,17 @@ fun makeInvisible(view: View, makeInvisibie: Boolean) {
     view.visibility = if (makeInvisibie) View.INVISIBLE else View.VISIBLE
 }
 
+@BindingAdapter("makeInvisibleIfStringBlank")
+fun makeInvisibleIfStringBlank(view: View, str: String?) {
+    view.visibility = if (str.isNullOrBlank()) View.INVISIBLE else View.VISIBLE
+}
+
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
+    if (imgUrl.isNullOrBlank()) {
+        imgView.setImageResource(0)
+        return
+    }
     imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
